5dccbeef243b431078065180eaab5a4759d36379
SISIRAK20 <93868334+SISIRAK20@users.noreply.github.com>
2021-12-08 15:46:52 +0530

    Add files via upload

--- co2/co2_10.py
+++ co2/co2_10.py
@@ -2,6 +2,6 @@ def factors(x):
     print("The factors of",x,"are:")
     for i in range(1, x + 1):
         if x % i == 0:
-        print(i)
+            print(i)
 n=int(input("Enter a number:"))
 factors(n)
---
+++ co2/co2_11.py
@@ -0,0 +1,8 @@
+import math
+trianglearea=lambda b,h:1/2*b*h
+rectarea=lambda l,b:l*b
+sqaurearea=lambda a:a*a
+
+print("area of triangle=",trianglearea(10,25))
+print("area of rectangle=",rectarea(35,45))
+print("area of square=",sqaurearea(18))
