d638d249020e66744c9332451302156a4f15a7b9
SISIRAK20 <93868334+SISIRAK20@users.noreply.github.com>
2021-12-20 12:55:14 +0530

    Add files via upload

---
+++ co3/statisticmod.py
@@ -0,0 +1,10 @@
+import statistics
+
+l1=[1,2,3,4,5]
+print(statistics.mean(l1))
+print(statistics.mean([1, 3, 5, 7, 9, 11])) #mean
+
+
+# Calculate harmonic mean
+print(statistics.harmonic_mean([40, 60, 80]))
+print(statistics.harmonic_mean([10, 30, 50, 70, 90]))
