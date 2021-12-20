daf1e857bfc6efefcac2c72f6a4ffa28d09afbb9
SISIRAK20 <93868334+SISIRAK20@users.noreply.github.com>
2021-12-20 12:57:01 +0530

    Add files via upload

---
+++ co3.py
@@ -0,0 +1,16 @@
+#help("modules")
+
+import math
+print("the value of pi is",math.pi)
+
+import math as m
+print("the value of pi is",m.pi)
+
+from math import pi,sqrt
+print("the value of pi is ",pi)
+print("square root of 4 is",sqrt(4))
+
+import math
+print(math.cos(90))
+print(math.sin(60))
+print(math.tan(45))
