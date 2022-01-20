9b2b241a818d83ecf2790e505350be7deb1d1a6a
SISIRAK20 <93868334+SISIRAK20@users.noreply.github.com>
2022-01-20 20:17:24 +0530

    Add files via upload

---
+++ co5/co5_1.py
@@ -0,0 +1,16 @@
+
+f1=open("myfile.txt","w")
+f1.write("This is my first file in python.\nWant to work with files.\nThis is my third")
+f1.close()
+
+f1=open("myfile.txt","r")
+f1.seek(0,0)
+ff=f1.readlines()
+for x in range(0,len(ff)):
+    print(ff[x])
+
+print()
+print(ff)
+
+f1.close()
+    
